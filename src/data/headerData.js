c2afd158ecf82449ed305216ec11b2c8826dd83d
annu308 <anindita308p@gmail.com>
2023-05-28 23:38:35 +0530

    Portfolio deploy on github

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Anindita Panda',
+    title: "Web Developer",
+    desciption:"A dedicated computer science student at USICT Delhi, pursuing MCA with a keen interest in software development. I am passionate about using my technical skills to create innovative software solutions. Equipped with a strong foundation in programming languages and development methodologies, constantly seeking new challenges to improve my skills.",
+    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    resumePdf: resume
+}
