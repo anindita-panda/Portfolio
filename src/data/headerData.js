60491b777c263965769da9c28084d3010454575c
Anindita Panda <77529823+anindita-panda@users.noreply.github.com>
2023-11-01 11:40:30 +0530

    year changed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import pic from '../assets/png/meprof.jpg'
 export const headerData = {
     name: 'Anindita Panda',
     title: "Web Developer",
-    desciption:"I love exploring new things! I'm a First Year MCA(SE) Student at USICT, Delhi. Primarily interested in Web Development. I enjoy learning new skills and implementing them in real life!",
+    desciption:"I love exploring new things! I'm a Second Year MCA(SE) Student at USICT, Delhi. Primarily interested in  UI/UX and Web Development. I enjoy learning new skills and implementing them in real life!",
 
     image: pic,
     resumePdf: resume
